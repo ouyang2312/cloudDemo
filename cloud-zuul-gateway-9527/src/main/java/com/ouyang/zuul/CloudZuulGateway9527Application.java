5e7170667c8e17c120872352f02e04094d697dca
ouyang <1262014533@qq.com>
2019-01-05 18:19:21 +0800

    zuul的使用

---
+++ cloud-zuul-gateway-9527/src/main/java/com/ouyang/zuul/CloudZuulGateway9527Application.java
@@ -0,0 +1,20 @@
+package com.ouyang.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy //开启Zuul
+public class CloudZuulGateway9527Application {
+
+    /**
+     * zuul会去eureka里面查找对应的服务 消费者没有注册到eureka，所以不能访问到
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZuulGateway9527Application.class, args);
+    }
+
+}
+
